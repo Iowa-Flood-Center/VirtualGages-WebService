4b8f776535fb27c5aef80dbd3e0dbfe0a9f88a70
virtualgages <virtualgages@s-iihr50.iihr.uiowa.edu>
2018-07-26 15:32:03 -0500

    frontend/code/site/libs/Settings.php

--- frontend/code/site/libs/Settings.php
+++ frontend/code/site/libs/Settings.php
@@ -1,7 +1,7 @@
 <?php
 
   class Settings{
-    const SETTINGS_FILE_NAME = "../../conf/settings.json";
+    const SETTINGS_FILE_NAME = "../../../conf/settings.json";
 	private static $attributes = NULL;
     
     /**
@@ -23,10 +23,24 @@
      * RETURN: none.
      */
     private static function read_file(){
+
+      // change folder
+      $old_folder_path = getcwd();
+      chdir(dirname(__FILE__));
+
       // read and parse file
+      if (!file_exists(Settings::SETTINGS_FILE_NAME)){
+        echo("File not found: ".Settings::SETTINGS_FILE_NAME."\n");
+        echo("  At:".getcwd()."\n");
+        echo("File:".__FILE__."\n");
+        exit(1);
+      }
       $file_text = file_get_contents(Settings::SETTINGS_FILE_NAME);
       Settings::$attributes = json_decode($file_text, true);
       
+      // get back to original folder
+      chdir($old_folder_path);
+
       // basic check
       if(is_null(Settings::$attributes)){
         echo("Failed reading file".PHP_EOL);
